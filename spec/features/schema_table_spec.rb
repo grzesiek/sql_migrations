97bc8f7b438442acb0b44a5d10e25bcd50f99884
Grzegorz Bizon <grzegorz.bizon@ntsn.pl>
2015-03-05 14:46:13 +0100

    Add migrations schema table test

--- spec/features/schema_table_spec.rb
+++ spec/features/schema_table_spec.rb
@@ -1,3 +1,13 @@
-describe 'schema table' do
-  pending 'todo'
+describe 'schema table in database' do
+  before(:each) do
+    allow(SqlMigrations::Database).to receive(:connect) { Sequel.sqlite }
+  end
+
+  it 'should create schema table if it does not exist' do
+    expect do
+      @database = SqlMigrations::Database.new(name: :default, 'adapter' => :sqlite)
+    end.to output("[+] Connected to database using sqlite adapter\n" +
+                  "[!] Installing `sqlmigrations_schema`\n").to_stdout
+    expect(@database.db.table_exists?(:sqlmigrations_schema)).to be true
+  end
 end
