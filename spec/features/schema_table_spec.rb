a5d20a20af6fd3392433f33760f65ec7bec333ba
Grzegorz Bizon <grzegorz.bizon@ntsn.pl>
2015-03-06 09:37:26 +0100

    Refactor and extend tests

--- spec/features/schema_table_spec.rb
+++ spec/features/schema_table_spec.rb
@@ -1,13 +1,26 @@
 describe 'schema table in database' do
-  before(:each) do
-    allow(SqlMigrations::Database).to receive(:connect) { Sequel.sqlite }
-  end
 
-  it 'should create schema table if it does not exist' do
+  it 'should be created if it does not exist' do
     expect do
       @database = SqlMigrations::Database.new(name: :default, 'adapter' => :sqlite)
     end.to output("[+] Connected to database using sqlite adapter\n" +
                   "[!] Installing `sqlmigrations_schema`\n").to_stdout
     expect(@database.db.table_exists?(:sqlmigrations_schema)).to be true
   end
+
+  it 'should not be create if it exists' do
+    @sqlite_db.create_table(:sqlmigrations_schema) do
+      primary_key :id
+      Bignum   :time
+      DateTime :executed
+      String   :name
+      String   :type
+      index [ :time, :type ]
+    end
+    expect do
+      @database = SqlMigrations::Database.new(name: :default, 'adapter' => :sqlite)
+    end.to output("[+] Connected to database using sqlite adapter\n").to_stdout
+    expect(@database.db.table_exists?(:sqlmigrations_schema)).to be true
+  end
+
 end
