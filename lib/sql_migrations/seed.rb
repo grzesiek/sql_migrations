7b993a6f0cd496696f209725cac369abd322cff0
Sebastian Korzeniecki <seba5zer@gmail.com>
2022-10-28 09:35:25 +0200

    Fix variable name, add default value for parameter

--- lib/sql_migrations/seed.rb
+++ lib/sql_migrations/seed.rb
@@ -2,7 +2,7 @@ module SqlMigrations
   # Seed class
   #
   class Seed < Script
-    def self.find(database_name, path_migration_seed)
+    def self.find(database_name, path_migration_seed = "")
       super(database_name, :seed, path_migration_seed)
     end
   end
